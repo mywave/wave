34a8a40e497651fb8fe0153a8c177e537097a1cd
frkochw <frkochw>
2004-01-06 13:43:27 +0000

    Initial revision

---
+++ deutsch.ads
@@ -0,0 +1,60 @@
+!
+!	@(#)deutsch.ads 03/05/08 11:43:37 1.2
+!
+Ausgabeformat:		Ausgabeformat: 
+Hintergrund:		Hintergrund: 
+Skalierung:		Skalierung: 
+geglaettet:		geglättet
+Balken:			Balken
+Windgeschwindigkeit:	Windgeschwindigkeit
+Keiner:			Keiner
+KeineRichtungen:	Keine Richtungen gefunden
+Mischen:		Mischen
+Grau:			Grau
+LaengenBreiten:		Längen und Breiten
+FFTRichtungen:		FFT Richtungen
+EindeutigeRichtungen:	Eindeutige Richtungen
+Drucken:		Drucken
+Analysenauswahl:	 Analysenauswahl
+Clear:			Lösche 
+Modell:			Modell
+Heading:		Heading: 
+ERSDaten:		/h/frkochw/ori/
+OKohneLG:		OK ohne LG
+BandBild:		 Bandnummer-Bildnummer <ENTER> eingeben 
+KeinStreifenName:	Kein Name für den SAR Streifen gewählt
+Ebene:			 Ebene
+Palette:		/h/frkochw/paletten/rainbow.hdf
+noSUNraster:		Dies ist kein SUN Rasterbild !
+oirms:			/h/frkochw/02/vs/
+Inhalt:			/h/frkochw/ERS/Band
+Wind:			 Windhistogramm
+WindVerteilung:		Windgeschwindigkeit über Wasser
+Gegenwind:		Gegenwind 
+Querwind:		Querwind 
+anpat1:			/h/frkochw/openimop/anpat1.dat
+anpat2:			/h/frkochw/openimop/anpat2.dat
+anpat3:			/h/frkochw/openimop/anpat3.dat
+anpat4:			/h/frkochw/openimop/anpat4.dat
+anpat5:			/h/frkochw/openimop/anpat5.dat
+powerloss1:		/h/frkochw/openimop/poloco1.dat
+powerloss2:		/h/frkochw/openimop/poloco21.dat
+RADARSATDaten:		/net/gmssun3/export/home/RADARSAT/
+RADARSATOutput:		/net/gmssun3/export/home2/ScanSAR/ori
+ASAR_XCA:		/h/frkochw/openimop/ASA_XCA_AXVIEC*
+ENVISATOutput:		/net/gmssun3/export/home2/ENVISAT/ori/
+Messungen:		/net/gmssun3/export/home2/frkochw/remo/messungen/messungen 
+ReMoWind:		/net/gmssun3/export/home2/frkochw/remo/remo000xe
+HiRLAMWind:		/net/gmssun3/export/home2/ScanSAR/HIRLAM_DAT/
+HiRLAMWindEs:		/net/gmssun3/export/home2/frkochw/scansar/v
+NOGABWind:		/net/gmssun3/export/home3/APL/
+SCATWind:		/net/gmssun3/export/home3/APL/
+Orbit:			Orbit: 
+Richtungeindeutigmachen:Richtungen eindeutig machen
+Modellanzeigen:		Modell anzeigen
+Tabelleausgeben:	Tabelle ausgeben
+Datensichern:		Daten sichern
+Hintergrundbild:	 Hintergrundbild
+Rechnername:		hostname
+MessungenOutput:	/net/gmssun21/export/home1/Wackerman/DAT/ori/
+CBandModell:		smo4v
